4e633e3e5243f3133e0197a56450d45ecb491ede
Your Actual Name <janethamrani@Janets-MacBook.fios-router.home>
2019-04-20 21:51:31 -0400

    added full name attribute to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_10_152336) do
+ActiveRecord::Schema.define(version: 2019_04_21_014450) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2019_04_10_152336) do
     t.string "canonical"
     t.string "author"
     t.string "publisher"
+    t.integer "user_id"
     t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
@@ -92,6 +93,7 @@ ActiveRecord::Schema.define(version: 2019_04_10_152336) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "full_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
