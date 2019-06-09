0f6614f6bf82ad32ea7dbd1f5be611b7f20b0406
Your Actual Name <janethamrani@Janets-MacBook.fios-router.home>
2019-06-09 19:34:56 -0400

    changes to layout and backend etc

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,19 @@ Rails.application.routes.draw do
   devise_for :admins
   mount Ckeditor::Engine => '/ckeditor'
 
+
   resources :users
   devise_for :users, controllers: {:registrations => "users/registrations"}
   resources :posts
   resources :projects
   resources :contacts, only: [:new, :create]
   resources :applies, only: [:new, :create]
+
+  devise_scope :user do
+  get 'login', to: 'devise/sessions#new'
+  get 'signout', to: 'devise/sessions#destroy', as: :destroy_user_session_path
+end
+
 
   get 'welcome/index'
   root 'welcome#index'
