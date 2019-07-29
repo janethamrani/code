e4e88697066e00505d778ede550c610bdca90432
Your Actual Name <janethamrani@Janets-MacBook.fios-router.home>
2019-07-28 22:07:08 -0400

    user controller stuff

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_action :find_user, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:show]
-  before_action :is_admin?, only: [:index, :new]
+  before_action :authenticate_user!, only: [:show, :edit]
+  before_action :is_admin?, only: [:index]
   load_and_authorize_resource
 
 
