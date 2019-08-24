0fb354c26eee9bdce5d88f158ddf13d8e62d84fb
Your Actual Name <janethamrani@Janets-MacBook.local>
2019-08-24 21:02:45 +0100

    added ckeditor-s3-paperclip

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_24_131123) do
+ActiveRecord::Schema.define(version: 2019_08_24_195706) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
